b751045684e52c38182cf7e363321574b7cab4c7
JiTeng <joyjiteng@gmail.com>
2018-08-29 08:34:37 +0800

    Update APISmokeTest.js

--- APISmokeTest.js
+++ APISmokeTest.js
@@ -56,9 +56,9 @@ function send(sub,tracelog,test_names_failures){
       port: 465, // SMTP 端口
       secureConnection: true, // 使用了 SSL
       auth: {
-        user: '844563792@qq.com',
+        user: 'XXX@qq.com',
         // 这里密码不是qq密码，是你设置的smtp授权码
-        pass: 'umxyndujkobxbejg',
+        pass: 'xxx',
       }
 	});
     
